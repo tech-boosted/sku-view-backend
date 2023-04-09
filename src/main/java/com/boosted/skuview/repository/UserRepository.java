4ab4b934fa777096262ef3ab7161f1aafe320168
Shahbaz <tech@boosted.in>
2023-04-09 19:15:01 +0530

    Deleted spring boot project

--- src/main/java/com/boosted/skuview/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.boosted.skuview.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.boosted.skuview.model.AppUser;
-
-public interface UserRepository extends JpaRepository<AppUser, Integer>  {
-	AppUser findByEmail(String email);
-}
